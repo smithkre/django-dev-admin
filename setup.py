24e66b271c9e5bc26ed94ad524733cc380cab884
smith.kre <smith.kre@gmail.com>
2021-05-09 15:22:04 +0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-querycount",
-    version="0.1.0",
+    version="0.1.1",
     author="Smith Krengkrud",
     author_email="smith.kre@gmail.com",
     description=("Allow you to login to any user via django command."),
