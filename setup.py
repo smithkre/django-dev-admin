d799e10286a297853fd376cde1c37d687ae28d7e
smith.kre <smith.kre@gmail.com>
2021-05-09 15:38:34 +0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="django-dev-admin",
-    version="0.1.1",
+    version="0.1.2",
     author="Smith Krengkrud",
     author_email="smith.kre@gmail.com",
     description=(
