e90c0428f27776d545a2efbdf030ff2c7217790c
smith.kre <smith.kre@gmail.com>
2021-05-09 15:20:53 +0700

    doc: update setup.py file and add bump2version

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="django-querycount",
+    version=__version__,
+    author="Smith Krengkrud",
+    author_email="smith.kre@gmail.com",
+    description=("Allow you to login to any user via django command."),
+    install_requires=[],
+    license="BSD 3",
+    keywords="django dev admin helper",
+    url="https://github.com/smithkre/django-dev-admin",
+    packages=[
+        "django_dev_admin",
+    ],
+    long_description="This project gives you a middleware that allow you to login to any user via django command.",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Utilities",
+    ],
+)
